14e23a0079b7b2ea4833cfe86e626e366bad213b
jchee <jasonchee@live.com>
2016-11-29 04:02:40 -0800

    with createAccount, generateBalanceSheet (dummy), generateProfitLossStatement (dummy)

--- AccountManagementSystemBoa/src/edu/boa/service/AccountManagementService.java
+++ AccountManagementSystemBoa/src/edu/boa/service/AccountManagementService.java
@@ -1,18 +1,26 @@
 package edu.boa.service;
 
 import java.io.File;
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import edu.boa.bo.Account;
+import edu.boa.bo.Transaction;
 import edu.boa.constants.AccountType;
 import edu.boa.dto.AccountSummary;
 import edu.boa.dto.BalanceSheet;
 import edu.boa.dto.Journal;
 import edu.boa.dto.Ledger;
 import edu.boa.dto.ProfitLossStatement;
+import edu.boa.exceptions.NotSameCurrencyException;
+import edu.boa.exceptions.SaveFailedException;
 import edu.boa.exceptions.UnbalancedTransactionException;
 import edu.boa.utils.Money;
 import edu.boa.utils.time.Time;
-
+import edu.boa.dao.AccountManagementDAO;
 /**
  * @author Boa
  */
@@ -29,10 +37,21 @@ public class AccountManagementService {
 	 * @param accountType 
 	 * @return
 	 */
-	public Account createAccount(String accountName, AccountType accountType) {
-		// TODO implement here
-		return null;
-	}
+   public boolean createAccount(String accountName, AccountType accountType)
+   {
+      Account a = new Account(accountName,
+            new Money("USD", new BigDecimal(0), "Dollars"), accountType);
+      //check for unique name and save account
+      try
+      {
+         AccountManagementDAO.getInstance().getAccount(accountName);
+         AccountManagementDAO.getInstance().saveAccount(a);
+      } catch (Exception e)
+      {
+         return false;
+      }
+      return true;
+   }
 
 	/**
 	 * @param accountName 
@@ -49,10 +68,25 @@ public class AccountManagementService {
 	 * @param amount 
 	 * @return
 	 */
-	public boolean transferFund(Account debitAccount, Account creditAccount, Money amount) throws UnbalancedTransactionException {
-		// TODO implement here
-		return false;
-	}
+   public boolean transferFund(Account debitAccount, Account creditAccount,
+         Money amount) throws UnbalancedTransactionException
+   {
+      Transaction t = new Transaction(debitAccount, creditAccount, amount,
+            new Time(Calendar.getInstance().getTimeInMillis()));
+      AccountManagementDAO d = AccountManagementDAO.getInstance();
+      //check 'equity = assets - liabilities'
+
+      
+      try
+      {
+         d.saveTransaction(t);
+      } catch (SaveFailedException e)
+      {
+         return false;
+      }
+      
+      return true;
+   }
 
 	/**
 	 * @param file 
@@ -98,8 +132,8 @@ public class AccountManagementService {
 	 * @return
 	 */
 	public ProfitLossStatement generateProfitLossStatement(Time fromDate, Time toDate) {
-		// TODO implement here
-		return null;
+	   return new  ProfitLossStatement( fromDate,  toDate, new HashMap<Account, Money>(), new HashMap<Account, Money>(),
+            new Money("USD", new BigDecimal(0), "Dollars"), new Money("USD", new BigDecimal(0), "Dollars") );
 	}
 
 	/**
@@ -108,7 +142,9 @@ public class AccountManagementService {
 	 * @return
 	 */
 	public BalanceSheet generateBalanceSheet(Time fromDate, Time toDate) {
-		// TODO implement here
-		return null;
+      return new  BalanceSheet( fromDate,  toDate, new HashMap<Account, Money>(), new HashMap<Account, Money>(),
+            new HashMap<Account, Money>(), new Money("USD", new BigDecimal(0), "Dollars"), new Money("USD", new BigDecimal(0), "Dollars") );
 	}
+	
+
 }
