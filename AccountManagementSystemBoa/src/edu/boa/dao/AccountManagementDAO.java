5ba0877411935dd55bbff7f1fe82590fdecce976
Sidharth Mishra <sidmishraw@gmail.com>
2016-11-29 01:14:54 -0800

    DAO backed by static datastructures for the demo.

--- AccountManagementSystemBoa/src/edu/boa/dao/AccountManagementDAO.java
+++ AccountManagementSystemBoa/src/edu/boa/dao/AccountManagementDAO.java
@@ -9,51 +9,94 @@ import edu.boa.exceptions.SaveFailedException;
 import edu.boa.utils.time.Time;
 
 /**
- * @author Boa
+ * @author sidmishraw
  */
 public class AccountManagementDAO {
+
+	private static AccountManagementDAO accountManagementDAO = null;
+
+	/**
+	 * Static datastructures to hold the Accounts and transactions
+	 * since no DB or IO is being used
+	 * Just of the demo
+	 */
+	private static Map<String, Account> accountStore 		= new HashMap<>();
+	private static Set<Transaction> transactionStore 		= new HashSet<>();
 
 	/**
 	 * Default constructor
 	 */
-	public AccountManagementDAO() {
+	private AccountManagementDAO() {}
+
+	/**
+	 * Singleton instance of the DAO
+	 * 
+	 * @return AccountManagementDAO
+	 */
+	public AccountManagementDAO getInstance() {
+
+		if ( null == accountManagementDAO ) {
+			
+			return new AccountManagementDAO();
+		}
+
+		return accountManagementDAO;
 	}
 
 	/**
 	 * @param account 
-	 * @return
+	 * @return boolean
 	 */
 	public boolean saveAccount(Account account) throws SaveFailedException {
-		// TODO implement here
-		return false;
+
+		accountStore.put(account.getID(), account);
+
+		return true;
 	}
 
 	/**
 	 * @param transaction 
-	 * @return
+	 * @return boolean
 	 */
 	public boolean saveTransaction(Transaction transaction) throws SaveFailedException {
-		// TODO implement here
-		return false;
+
+		transactionStore.add(transaction);
+
+		return true;
 	}
 
 	/**
-	 * @param accountName 
-	 * @return
+	 * @param accountID
+	 * @return Account
 	 */
-	public Account getAccount(String accountName) throws AccountNotFoundException {
-		// TODO implement here
-		return null;
+	public Account getAccount(String accountID) throws AccountNotFoundException {
+
+		if ( null == accountStore.get(accountID) ) {
+
+			throw new AccountNotFoundException("Account was not found.");
+		}
+
+		return accountStore.get(accountID);
 	}
 
 	/**
 	 * @param fromDate 
 	 * @param toDate 
-	 * @return
+	 * @return Set<Transaction>
 	 */
 	public Set<Transaction> getTransactions(Time fromDate, Time toDate) throws Exception {
-		// TODO implement here
-		return null;
+
+		Set<Transaction> transactions = new HashSet<>();
+
+		for ( Transaction transaction : transactionStore ) {
+
+			if ( transaction.getDate().compareTo(fromDate) >= 0 && transaction.getDate().compareTo(toDate) <= 0 ) {
+
+				transactions.add(transaction);
+			}
+		}
+
+		return transactions;
 	}
 
 }
